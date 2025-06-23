6e1fdb56abfb6807169d6678c8aabd5b7585a02c
NzuCRAS <140897356+NzuCRAS@users.noreply.github.com>
2025-06-23 15:18:54 +0800

    Delete rpc-project directory

--- rpc-project/src/main/java/com/example/rpc/registry/ServiceDiscovery.java
+++
@@ -1,9 +0,0 @@
-package com.example.rpc.registry;
-
-import java.util.List;
-
-public interface ServiceDiscovery {
-    String discover(String serviceName) throws Exception;
-    void close() throws Exception;
-    public List<String> getService(String serviceName) throws Exception;
-}
